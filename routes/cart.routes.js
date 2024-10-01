fe08bd2e8cdfd4360b56c81e913dc76344f1bf39
faks <87184157+fran-galic@users.noreply.github.com>
2024-10-01 14:43:20 +0200

    dodani fajlovi

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 
-function getName(id, cart){
+function dobiIme(id, cart){
     for(var i = 0; i < cart.length; i++){
         if(cart[i].id == id){
             return cart[i].name;
@@ -11,7 +11,7 @@ function getName(id, cart){
     }
 }
 
-function getCart(req){
+function dobiCart(req){
    var cart = req.session.cart;
    if(!cart){
        cart = req.session.cart = [];
@@ -22,22 +22,22 @@ function getCart(req){
 router.get('/getAll', function(req, res){
     res.render('cart', 
         {title: 'Citadel - cart',
-        cart: getCart(req)});
+        cart: dobiCart(req)});
 });
 
 router.post('/add/:id', function(req, res){
-    var cart = getCart(req);
+    var cart = dobiCart(req);
     let search = cart.find((p) => p.id === req.params.id);
     if (search === undefined) {
-        cart.push({ id: req.params.id, name: getName(req.params.id, cart), col: 1});
+        cart.push({ id: req.params.id, name: dobiIme(req.params.id, cart), col: 1});
     } else {
         search.col += 1;
     }
     res.redirect('/cart/getAll');
 });
 
-router.post('/remove/:id', function(req, res, next){
-    var cart = getCart(req);
+router.post('/remove/:id', function(req, res){
+    var cart = dobiCart(req);
     let search = cart.find((p) => p.id === req.params.id);
     if (search.col == 1) {
         search.col -= 1;
